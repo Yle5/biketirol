b646ac5ee3e3e2bbc90d1d425c8001b260e3877e
Ylenia <05ylenia@gmail.com>
2024-05-23 17:15:41 +0200

    add selected for own etappe

--- main.js
+++ main.js
@@ -69,5 +69,9 @@ controlElevation.load("data/etappe32.gpx");
 let pulldown = document.querySelector("#pulldown"); 
 
 for (let etappe of ETAPPEN) {
-    pulldown.innerHTML += `<option value="${etappe.user}">Etappe ${etappe.nr}: ${etappe.titel}</option>`;  
+    let status = ""; 
+    if (etappe.nr == 32) {
+        status = " selected "; 
+    }
+    pulldown.innerHTML += `<option ${status} value="${etappe.user}">Etappe ${etappe.nr}: ${etappe.titel}</option>`;  
 }
